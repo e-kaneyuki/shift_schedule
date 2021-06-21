ed99634f0eace5bd220fc00c22259081e7deec76
e-kaneyuki <occultchikin@gmail.com>
2021-06-21 18:41:51 +0900

    usersテーブルのカラム名修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_19_135931) do
+ActiveRecord::Schema.define(version: 2021_06_21_084612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2021_06_19_135931) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "password_digest", null: false
-    t.string "communitty_id", null: false
+    t.string "community_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
