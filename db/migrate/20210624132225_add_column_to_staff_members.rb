2bdbb468b4a6c3812314780055022c44d84f3069
e-kaneyuki <occultchikin@gmail.com>
2021-06-25 00:11:39 +0900

    rubocop対応としてmigrationファイルのadd_columnsをchange_tableへまとめた

--- db/migrate/20210624132225_add_column_to_staff_members.rb
+++ db/migrate/20210624132225_add_column_to_staff_members.rb
@@ -1,9 +1,11 @@
 class AddColumnToStaffMembers < ActiveRecord::Migration[6.1]
   def change
-    add_column :staff_members, :desired_date_of_work, :datetime, null: true
-    add_column :staff_members, :desired_holiday_date, :datetime, null: true
-    add_column :staff_members, :the_number_of_paid_holidays, :float, null: false
-    add_column :staff_members, :possible_continuous_work, :integer, null: false
-    add_column :staff_members, :hope_for_consecutive_holidays, :integer, null: false
+    change_table :staff_members, bulk: true do |t|
+      t.datetime :desired_date_of_work
+      t.datetime :desired_holiday_date
+      t.float :the_number_of_paid_holidays, null: false, default: ""
+      t.integer :possible_continuous_work, null: false, default: ""
+      t.integer :hope_for_consecutive_holidays, null: false, default: ""
+    end
   end
 end
