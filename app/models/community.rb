44c9a5c6952db382a2e35111ae65fb31e9f340a6
e-kaneyuki <occultchikin@gmail.com>
2021-07-16 21:35:39 +0900

    validationの設定

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,4 +1,6 @@
 class Community < ApplicationRecord
   has_many :users, through: :community_users
   has_many :shift_calendars, dependent: :delete_all
+
+  validates :communities_name, presence: true, uniqueness: true, length: { maximum: 20 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :communities, through: :community_users
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :password_digest, presence: true, length: { minimum: 6 }
+  validates :communitty_id, presence: true, uniqueness: true, numericality: { only_integer: true }
 end
