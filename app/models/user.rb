e3195402ea41f8e80b7ff28913747434622a03c1
e-kaneyuki <occultchikin@gmail.com>
2021-07-19 14:48:15 +0900

    Userモデルのpasswordに関するvalidationを削除しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
   has_many :communities, through: :community_users
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
-  validates :password_digest, presence: true, length: { minimum: 6 }
   validates :community_id, presence: true, numericality: { only_integer: true }
 end
