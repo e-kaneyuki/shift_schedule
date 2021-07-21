74431aaed0dd4c34530eee6ba14ca5a51ed860c6
e-kaneyuki <occultchikin@gmail.com>
2021-07-21 22:30:48 +0900

    community_idはUserテーブルに直打ちしないので消した

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :communities, through: :community_users
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
-  validates :community_id, presence: true, numericality: { only_integer: true }
+  # validates :community_id, presence: true, numericality: { only_integer: true }
 end
