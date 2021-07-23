e5fdf0022021dc7868c7f2e4830629bd8c9e73b6
e-kaneyuki <occultchikin@gmail.com>
2021-07-24 01:42:19 +0900

    Userテーブルのカラムを編集,ダミーデータの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :user do
     sequence(:name) { Faker::JapaneseMedia::Doraemon.character}
-    sequence(:email) { |n| "TEST#{n}@example.com"}
+    sequence(:email) { Faker::Internet.email }
     sequence(:password) { Faker::Internet.password(min_length: 6, max_length: 20) }
-    sequence(:community) { Faker::JapaneseMedia::Doraemon.gadget }
-    sequence(:community_id) { Faker::Number.between(from: 1, to: 30) }
+    # sequence(:community) { Faker::JapaneseMedia::Doraemon.gadget }
   end
 end
