a68a447eaa1142f4cd34593609ac01db911cd1a0
e-kaneyuki <occultchikin@gmail.com>
2021-07-26 00:28:26 +0900

    異常系テストも実装する必要があるのでダミーデータの文字数制限を解除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,10 @@ FactoryBot.define do
   factory :user do
     name { Faker::JapaneseMedia::Doraemon.character }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6, max_length: 8) }
+    password { Faker::Internet.password }
+
+    # deviseのvalidationでパスワードは6~128桁に設定されている
+    # config.password_length = 6..128
+    # password { Faker::Internet.password(min_length: 6, max_length: 8) }
   end
 end
