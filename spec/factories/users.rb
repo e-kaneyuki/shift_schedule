52bd9e55a8abf4c82d84b41a7cf408ac5768a103
e-kaneyuki <occultchikin@gmail.com>
2021-07-25 19:01:41 +0900

    password管理をdb上ではなくDviseで行えるようdbからカラムを削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :user do
-    sequence(:name) { Faker::JapaneseMedia::Doraemon.character}
-    sequence(:email) { Faker::Internet.email }
-    sequence(:password) { Faker::Internet.password(min_length: 6, max_length: 20) }
-    # sequence(:community) { Faker::JapaneseMedia::Doraemon.gadget }
+    name { Faker::JapaneseMedia::Doraemon.character }
+    email { Faker::Internet.email }
+    password { 'password' }
   end
 end
