c7c7c44fb93b18dd553509ed09ada49a07b035f0
e-kaneyuki <occultchikin@gmail.com>
2021-07-25 20:30:15 +0900

    factorybotパスワード自動生成できるよう修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     name { Faker::JapaneseMedia::Doraemon.character }
     email { Faker::Internet.email }
-    password { 'password' }
+    password { Faker::Internet.password(min_length: 6, max_length: 8) }
   end
 end
