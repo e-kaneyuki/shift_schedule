2b9c41d3337789b22e352da8167e57c6fe77866c
e-kaneyuki <occultchikin@gmail.com>
2021-07-26 22:28:01 +0900

    テストコードを一旦保存

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,32 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  binding.pry
-  # describe '' do
-  #   context '' do
-  #     it "" do
-  #       expect().to
-  #     end
-  #   end
-  # end
+  # 正常系
+  describe '名前' do
+    let(:user) { FactoryBot.build(:user) }
+    context 'が入力されている場合' do
+      it "登録できる" do
+        expect(user.name).not_to eq nil
+      end
+    end
+    context 'が30文字以内の場合' do
+      it '登録できる' do
+        expect(user.name.length).to be <= 30
+      end
+    end
+
+    # context 'が重複していない場合' do
+    #   it '登録できる' do
+    #     # expect(user.name). ここに重複しないことを書きたい
+    #   end
+    # end
+
+  end
 end
+# end
+#       # email
+#       # expect(user.email).to
+#       # # password
+#       # expect(user.password).those
+#      # 異常系
+# end
