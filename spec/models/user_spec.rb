52ce953ea9be6dd1413f85e23e42f6c864a4f892
e-kaneyuki <occultchikin@gmail.com>
2021-07-28 20:54:14 +0900

    1 example に対し4 assertionまで対応可能へ変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,6 +51,7 @@ RSpec.describe User, type: :model do
         expect(user.email).not_to eq nil
       end
     end
+
     context "メールアドレスが入力されていない場合" do
       it "登録できない" do
         user = User.new(name: "kanechan", email: "", password: "password")
@@ -58,67 +59,67 @@ RSpec.describe User, type: :model do
         expect(user.errors[:email]).to include("can't be blank")
       end
     end
+
     context "メールアドレスが重複している場合" do
       it "登録できない" do
-          User.create!(name: "kinchan", email: "kinchan@example.com", password: "password")
-          user = User.new(name: "kanechan", email: "kinchan@example.com", password: "password")
-          expect(user).to be_invalid
-          expect(user.errors[:email]).to include("has already been taken")
+        User.create!(name: "kinchan", email: "kinchan@example.com", password: "password")
+        user = User.new(name: "kanechan", email: "kinchan@example.com", password: "password")
+        expect(user).to be_invalid
+        expect(user.errors[:email]).to include("has already been taken")
       end
     end
   end
 
   describe "パスワードのバリデーションテスト" do
-      let(:user) { FactoryBot.build(:user) }
-      context "パスワードが入力されている場合" do
-        it "登録できる" do
-          expect(user.password).not_to eq nil
-        end
+    let(:user) { FactoryBot.build(:user) }
+    context "パスワードが入力されている場合" do
+      it "登録できる" do
+        expect(user.password).not_to eq nil
       end
+    end
 
-      context "パスワードが入力されていない場合" do
-        it "登録できない" do
-          user = User.new(name: "kinchan", email: "kinchan@example.com", password: "")
-          expect(user).to be_invalid
-          expect(user.errors[:password]).to include("can't be blank")
-        end
+    context "パスワードが入力されていない場合" do
+      it "登録できない" do
+        user = User.new(name: "kinchan", email: "kinchan@example.com", password: "")
+        expect(user).to be_invalid
+        expect(user.errors[:password]).to include("can't be blank")
       end
+    end
 
-      context "パスワードが5文字以内の場合" do
-        it "登録できない" do
-          user = User.new(name: "kinchan", email: "foo@example.com", password: "a" * 5)
-          expect(user).to be_invalid
-          expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
-        end
+    context "パスワードが5文字以内の場合" do
+      it "登録できない" do
+        user = User.new(name: "kinchan", email: "foo@example.com", password: "a" * 5)
+        expect(user).to be_invalid
+        expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
       end
+    end
 
-      context "パスワードが6文字以上128文字以内の場合" do
-        it "登録できる" do
-          user = User.new(name: "kinchan", email: "foo@example.com", password: "a" * 6)
-          expect(user).to be_valid
-          expect(user.password.length).to be >= 6
+    context "パスワードが6文字以上128文字以内の場合" do
+      it "登録できる" do
+        user = User.new(name: "kinchan", email: "foo@example.com", password: "a" * 6)
+        expect(user).to be_valid
+        expect(user.password.length).to be >= 6
 
-          another_user = User.new(name: "kinchan", email: "foo@example.com", password: "a" * 128)
-          expect(another_user).to be_valid
-          expect(another_user.password.length).to be <= 128
-        end
+        another_user = User.new(name: "kinchan", email: "foo@example.com", password: "a" * 128)
+        expect(another_user).to be_valid
+        expect(another_user.password.length).to be <= 128
       end
+    end
 
-      context "パスワードが129文字以上の場合" do
-        it "登録できない" do
-          user = User.new(name: "kinchan", email: "foo@example.com", password: "a" * 129)
-          expect(user).to be_invalid
-          expect(user.errors[:password]).to include("is too long (maximum is 128 characters)")
-        end
+    context "パスワードが129文字以上の場合" do
+      it "登録できない" do
+        user = User.new(name: "kinchan", email: "foo@example.com", password: "a" * 129)
+        expect(user).to be_invalid
+        expect(user.errors[:password]).to include("is too long (maximum is 128 characters)")
       end
+    end
 
-      context "パスワードが重複している場合" do
-        it "登録できない" do
-          User.create!(name: "kinchan", email: "kinchan@example.com", password: "password")
-          user = User.new(name: "kinchan", email: "kanechan@example.com", password: "password")
-          expect(user).to be_invalid
-          expect(user.errors[:name]).to include("has already been taken")
-        end
+    context "パスワードが重複している場合" do
+      it "登録できない" do
+        User.create!(name: "kinchan", email: "kinchan@example.com", password: "password")
+        user = User.new(name: "kinchan", email: "kanechan@example.com", password: "password")
+        expect(user).to be_invalid
+        expect(user.errors[:name]).to include("has already been taken")
       end
     end
   end
