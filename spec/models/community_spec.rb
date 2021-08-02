798ac69ee862683bca4e1961f3abdb61f2054359
e-kaneyuki <occultchikin@gmail.com>
2021-08-02 20:52:54 +0900

    コミュニティー名communities_name, presence: trueのテスト

--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -11,8 +11,22 @@
 #
 #  index_communities_on_communities_name  (communities_name) UNIQUE
 #
-# require "rails_helper"
+require "rails_helper"
 
-# RSpec.describe Community, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Community, type: :model do
+  describe "コミュニティー名のバリデーションテスト" do
+    context "コミュニティー名が入力されている場合" do
+      it "登録できる" do
+        community = Community.new(communities_name: "岡山病院" )
+        expect(community.communities_name).not_to eq nil
+      end
+    end
+    context "コミュニティー名が入力されていない場合" do
+      it "登録できない" do
+        another_community = Community.new(communities_name: "" )
+        expect(another_community).to be_invalid
+        expect(another_community.errors[:communities_name]).to include("can't be blank")
+      end
+    end
+  end
+end
