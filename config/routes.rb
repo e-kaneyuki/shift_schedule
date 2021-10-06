41463f68d63335a2190ac6a3ce16dab02b1497e7
e-kaneyuki <occultchikin@gmail.com>
2021-10-06 20:00:02 +0900

    devise controller,viewをgenerate

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   root 'homes#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
