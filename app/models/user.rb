0d9c752e492f01de90fd7bd9dd4d09c639ad6a49
e-kaneyuki <occultchikin@gmail.com>
2021-10-06 23:24:51 +0900

    Validation修正 〜devise使用に合わせてUserのnameを削除〜

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@
 #  id                     :bigint           not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  name                   :string           not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
@@ -15,7 +14,6 @@
 # Indexes
 #
 #  index_users_on_email                 (email) UNIQUE
-#  index_users_on_name                  (name) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
@@ -25,5 +23,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :communities, through: :community_users
 
-  validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
+  # validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
 end
