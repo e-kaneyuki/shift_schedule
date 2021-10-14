3dd142cde53a377bb327e669b144e0966b6ecc4f
e-kaneyuki <occultchikin@gmail.com>
2021-10-15 06:20:19 +0900

    user tableにname columnを追加

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_07_25_095858) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
