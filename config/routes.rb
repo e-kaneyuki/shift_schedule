445fc251de829abaef0b0b47e7024808dc5e6436
e-kaneyuki <occultchikin@gmail.com>
2021-10-17 20:16:11 +0900

    shift_calenderのルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # resources :leader, only: [:show]
   get "leaders/show"
   root "homes#index"
-
+  resources :shift_calender
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
